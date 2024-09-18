b2302ffa6ba1785e72dbd0abea8372f594b977eb
Anders <Andersravn94@gmail.com>
2024-09-18 09:55:38 +0200

    Update StudentRepositoryTest.java

--- src/test/java/com/example/jpastudent/repositories/StudentRepositoryTest.java
+++ src/test/java/com/example/jpastudent/repositories/StudentRepositoryTest.java
@@ -1,3 +1,5 @@
+/*
+
 package com.example.jpastudent.repositories;
 
 import com.example.jpastudent.model.Student;
@@ -7,19 +9,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class StudentRepositoryTest {
 
-   @Autowired
-   StudentRepository studentRepository;   // field injection as constructor injection is not possible in test classes
+    @Autowired
+    StudentRepository studentRepository;   // field injection as constructor injection is not possible in test classes
 
     @Test
     void testOneBruce() {
         List<Student> lst  = studentRepository.findByName("Bruce");
         assertEquals(1, lst.size());
     }
-
-
-
 }
+
+*/
